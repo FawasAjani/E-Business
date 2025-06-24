849425917182c7974c669ed33bcc001547e89df6
Fawas Ajani <G00413222@atu.ie>
2025-06-24 13:25:17 +0100

    DynamicPortfolio

--- frontend/src/Portfolio/DynamicPortfolio.js
+++ frontend/src/Portfolio/DynamicPortfolio.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';//react-router-dom
 import axios from 'axios';//axios
-
+//DynamicPortfolio
 const DynamicPortfolio = () => {
     const { id } = useParams();
     const [portfolio, setPortfolio] = useState('');
