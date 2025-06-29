536fdee968bc299e2b2bbdac60901d08de4fdaa0
Fawas Ajani <G00413222@atu.ie>
2025-06-29 12:28:57 +0100

    // QrcodeGenerator

--- frontend/src/templates/Professional.js
+++ frontend/src/templates/Professional.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './Professional.css';
+import './Professional.css';//professional
 import QrCodeGenerator from '../components/QrCodeGenerator';//QrcodeGenerator
 import imageCompression from 'browser-image-compression';//imageCompression
 
