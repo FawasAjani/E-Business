43e3f31e91ac13aa797e74a4534c0d5750514166
Fawas Ajani <G00413222@atu.ie>
2025-06-29 13:15:47 +0100

    //service3

--- frontend/src/templates/Freelancer.js
+++ frontend/src/templates/Freelancer.js
@@ -53,7 +53,7 @@ const Freelancer = () => {
                 setSkill3(response.data.freelance.skill3 || "Enter skill 3");//Skill3
                 setService1(response.data.freelance.service1 || "Enter service 1");//Service1
                 setService2(response.data.freelance.service2 || "Enter service 2");//Service2
-                setService3(response.data.freelance.service3 || "Enter service 3");
+                setService3(response.data.freelance.service3 || "Enter service 3");//service3
                 setProjectName1(response.data.freelance.projectName1 || "Enter project 1 name");
                 setProjectDescription1(response.data.freelance.projectDescription1 || "Enter project 1 description");
                 setProjectLink1(response.data.freelance.projectLink1 || "Enter project 1 link");
